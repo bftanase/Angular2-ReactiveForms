42a153778d44ad8b2bcd00851b46dbf3c69fbea2
Bogdan Tanase <florin-bogdan.tanase@ubisoft.com>
2017-07-27 18:53:53 +0300

    tutorial complete

--- Demo-Start-Updated/src/app/customers/customer.component.ts
+++ Demo-Start-Updated/src/app/customers/customer.component.ts
@@ -1,7 +1,33 @@
 import {Component, OnInit} from '@angular/core';
 
 import {Customer} from './customer';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import 'rxjs/add/operator/debounceTime';
+import {AbstractControl, FormArray, FormBuilder, FormGroup, ValidatorFn, Validators} from '@angular/forms';
+
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): {[key: string]: boolean} | null => {
+    if (c.value !== undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
+      return {'range': true};
+    }
+
+    return null;
+  };
+}
+
+function emailMatcher(c: AbstractControl) {
+  let emailControl = c.get('email');
+  let confirmControl = c.get('confirmEmail');
+
+  if (emailControl.pristine || confirmControl.pristine) {
+    return null;
+  }
+
+  if (emailControl.value === confirmControl.value) {
+    return null;
+  }
+
+  return { 'match': true};
+}
 
 @Component({
   selector: 'my-signup',
@@ -9,8 +35,17 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
-
   customer: Customer = new Customer();
+  emailMessage: string;
+
+  get addresses(): FormArray {
+    return <FormArray>this.customerForm.get('addresses');
+  }
+
+  private validationMessages = {
+    required: 'Please confirm your email address.',
+    pattern: 'Please enter a valid email address.'
+  };
 
   constructor(private fb: FormBuilder) {
   }
@@ -19,16 +54,48 @@ export class CustomerComponent implements OnInit {
     this.customerForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
-      email: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+')]],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+')]],
+        confirmEmail: ['', Validators.required],
+      }, {validator: emailMatcher}),
       sendCatalog: true,
       phone: '',
-      notification: 'email'
+      notification: 'email',
+      rating: ['', ratingRange(1, 7)],
+      addresses: this.fb.array([this.buildAddress()])
+    });
+
+    this.customerForm.get('notification').valueChanges.subscribe(value => this.setNotification(value));
+
+    const emailControl = this.customerForm.get('emailGroup.email');
+    emailControl.valueChanges.debounceTime(1000).subscribe(value => this.setMessage(emailControl));
+  }
+
+  addAddress(): void {
+    this.addresses.push(this.buildAddress());
+  }
+
+  buildAddress(): FormGroup {
+    return this.fb.group({
+      addressType: 'home',
+      street1: '',
+      street2: '',
+      city: '',
+      state: '',
+      zip: ''
     });
   }
 
   save() {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  setMessage(c: AbstractControl): void {
+    this.emailMessage = '';
+    if ((c.touched || c.dirty) && c.errors) {
+      this.emailMessage = Object.keys(c.errors).map(key => this.validationMessages[key]).join(' ');
+    }
   }
 
   populateTestData(): void {
